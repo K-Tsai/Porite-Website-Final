03adbffc417ce403e01258ed141bac1fc929aa93
Kevin Tsai <k.tsai92@gmail.com>
2020-05-27 21:13:13 -0400

    added products

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -4,10 +4,67 @@ import "./Products.css"
 class Products extends Component {
   render() {
     return (
-      <div id="Products">
-        <div id="page-wrap">
-          <h1>Products</h1>
-          <h2>Check out the products page with our numerous parts</h2>
+      <div className="container">
+        <div className="row">
+          <h1 className="autoParts">Auto Parts</h1>
+          <div className="parts1">
+            <div className="col">
+              <img 
+                alt="Seat Adjustment Parts"
+                src={SeatAdjustment}
+                id="seatAdjustment"
+                class="parts"
+              />
+              <h2>Seat Adjustment Parts</h2>
+            </div>
+            <div className="col">
+              <img 
+                alt="Steel"
+                src={Steel}
+                id="steelParts"
+                class="parts"
+              />
+              <h2>Stainless Steel Parts</h2>
+            </div>
+            <div className="col">
+              <img 
+                alt="Steering Parts"
+                src={Steering}
+                id="steeringParts"
+                class="parts"
+              />
+              <h2>Steering Wheel Parts</h2>
+            </div>
+          </div>
+          <div className="parts2">
+            <div className="col">
+              <img 
+                alt="Synchronizer"
+                src={Synchronizer}
+                id="synchronizerParts"
+                class="parts"
+              />
+              <h2>Synchronizer Hubs</h2>
+            </div>
+            <div className="col">
+              <img 
+                alt="Transmission"
+                src={Transmission}
+                id="transmissionParts"
+                class="parts"
+              />
+              <h2>Transmission Parts</h2>
+            </div>
+            <div className="col">
+              <img 
+                alt="VVT"
+                src={VVT}
+                id="VVTparts"
+                class="parts"
+              />
+              <h2>Transmission Parts</h2>
+            </div>
+          </div>
         </div>
       </div>
     );
