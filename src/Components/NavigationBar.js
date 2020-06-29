cc71f39e9fe9435b0da2c40f6811738051d7f2de
Kevin Tsai <k.tsai92@gmail.com>
2020-06-29 14:27:09 -0400

    added fixes to NavBar

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Nav, Navbar} from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar'
 import PoriteLogo from '../pictures/PoriteLogo.png';
 import {Link} from 'react-router-dom';
 import "./NavigationBar.css"
@@ -7,18 +8,16 @@ class NavigationBar extends React.Component {
   render() {
     return (
       <div className ="container">
-        <Navbar inverse="true" fixedtop="true" expand="lg" bg="#EA2900" variant="light">
-          <Link to= "/">
-            <Navbar.Brand>
+        <Navbar collapseOnSelect light expand="lg" bg="#EA2900">
+            <Navbar.Brand href="/">
               <img
                 alt="Logo"
                 src={PoriteLogo}
                 id="poriteLogo"
                 className="d-inline-block align-top"/>
             </Navbar.Brand>
-          </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="toggler" />
+        <Navbar.Collapse id="responsive-navbar-nav justify-content-end">
           <Nav className="mr-auto">
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/products">Products</Nav.Link>
